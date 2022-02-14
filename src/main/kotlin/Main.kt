dd164aa89f00ddf055bf0bb83923ae55d26f818d
evan <evansullivan74@gmail.com>
2022-02-14 19:14:14 +0000

    final commit of each file to make sure each file is on github

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,6 @@ import java.util.*
 private val input = Scanner(System.`in`);
 private val scrabbleScore = ScrabbleScore();
 
-//TODO This program has bugs and incomplete code in it...you will need to fix them so you have the following functionality:
-// The main method should:
-//    - print out the letterValues to the console
-//    - ask the user to enter a word
-//    - display the Scrabble Score for the word (the case of the letters shouldn't matter i.e. u is the same as U)
-//    - the user should be continually asked to enter a word until they type a sentinel value (e.g. -1) to end the program
 
 fun main(args: Array<String>) {
    println( scrabbleScore.letterValues())
